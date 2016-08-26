20c0b51d9fe0e7f6aa12afbb99e1e38a5c5ecdf5
powerMil <miltos.miltiadous@st.ouc.ac.cy>
2016-08-26 11:54:32 +0300

    Remove extra colons from the produced json file

--- src/main/java/eu/esit4sip/tools/bsslurper/Main.java
+++ src/main/java/eu/esit4sip/tools/bsslurper/Main.java
@@ -5,8 +5,8 @@ import javax.swing.text.html.HTML;
 public class Main {
 
     public static void main(String[] args) throws Throwable {
-        Html_to_json_tags_version.main(args);
-        Web_to_json_parser.main(args);
-        Web_to_json_parser_ver2.main(args);
+        HtmlToJsonTagsParser_ver_1_0.main(args);
+        WebToJsonParser_ver_1_0.main(args);
+        WebToJsonParser_ver_1_1.main(args);
     }
 }
